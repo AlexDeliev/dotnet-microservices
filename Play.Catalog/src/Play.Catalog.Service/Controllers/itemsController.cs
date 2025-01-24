ac9fd5746f01dde9d10d6bfc58c4dbe08a4fac03
Alex Deliev <alexdeliev7@gmail.com>
2025-01-24 19:07:42 +0200

    Initialize itemsController.cs

--- Play.Catalog/src/Play.Catalog.Service/Controllers/itemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/itemsController.cs
@@ -35,12 +35,46 @@ namespace Play.Catalog.Service.Controllers
             return item;
         }
 
+        // POST /items
         [HttpPost]
         public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
         {
             var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
             items.Add(item);
             return CreatedAtAction(nameof(Get), new { id = item.Id }, item);
+        }
+
+        // PUT /items/{id}
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+            var updatedItem = existingItem with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price
+            };
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            items[index] = updatedItem;
+            return NoContent();
+        }
+
+        // DELETE /items/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            if (index == -1)
+            {
+                return NotFound();
+            }
+            items.RemoveAt(index);
+            return NoContent();
         }
     }
 }
