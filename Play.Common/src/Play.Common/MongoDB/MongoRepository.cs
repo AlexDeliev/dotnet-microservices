8754a78de4300139e0c2245ac95ec3d02645bcb6
Alex Deliev <alexdeliev7@gmail.com>
2025-01-26 22:12:56 +0200

    Add MongoDB repository method

---
+++ Play.Common/src/Play.Common/MongoDB/MongoRepository.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Play.Common.MongoDB
+{
+    public class MongoRepository<T> : IRepository<T> where T : IEntity
+    {
+        private readonly IMongoCollection<T> dbCollection;
+
+        private readonly FilterDefinitionBuilder<T> filterBuilder = Builders<T>.Filter;
+
+        // The constructor initializes the items collection in the MongoDB database.
+        public MongoRepository(IMongoDatabase database, string collectionName)
+        {
+            dbCollection = database.GetCollection<T>(collectionName);
+        }
+
+        // The GetAllAsync method retrieves all items from the items collection.
+        public async Task<IReadOnlyCollection<T>> GetAllAsync()
+        {
+            return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+
+        // The GetAsync method retrieves an item from the items collection by its ID.
+        public async Task<T> GetAsync(Guid id)
+        {
+            FilterDefinition<T> filter = filterBuilder.Eq(entity => entity.Id, id);
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        // The CreateAsync method inserts a new item into the items collection.
+        public async Task CreateAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            await dbCollection.InsertOneAsync(entity);
+        }
+
+        // The UpdateAsync method updates an item in the items collection.
+        public async Task UpdateAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            FilterDefinition<T> filter = filterBuilder.Eq(existingEntity => existingEntity.Id, entity.Id);
+            await dbCollection.ReplaceOneAsync(filter, entity);
+        }
+
+        // The RemoveAsync method removes an item from the items collection by its ID.
+        public async Task RemoveAsync(Guid id)
+        {
+            FilterDefinition<T> filter = filterBuilder.Eq(entity => entity.Id, id);
+            await dbCollection.DeleteOneAsync(filter);
+        }
+    }
+}
